245664d2fb933a48c56bc53f87a4ef1b85f4059e
dajun928 <1663177102@qq.com>
2019-03-05 00:14:18 +0800

    add some tool
    add some tool

---
+++ helloworld.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+"""
+@version : 2.7.14
+@file : helloworld.py
+@time : 2019/03/05 00:02:07
+@func : 我的自动注释样例
+"""
+import platform
+
+print(platform.python_version())
