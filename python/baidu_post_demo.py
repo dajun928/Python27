556d5227094dec848bd563aa0bf65f8597acd4d6
dajun928 <1663177102@qq.com>
2019-04-28 22:33:03 +0800

    add

---
+++ python/baidu_post_demo.py
@@ -0,0 +1,45 @@
+# coding: utf-8
+
+import httplib
+import md5
+import urllib
+import random
+import requests
+
+'''
+http://api.fanyi.baidu.com/api/trans/product/desktop?req=developer
+
+'''
+
+appid = '20190425000291658'  # 你的appid
+secretKey = 'yhNR5rFuuodstkFVVq6H'  # 你的密钥
+
+httpClient = None
+myurl = 'http://api.fanyi.baidu.com/api/trans/vip/translate'
+q = 'hello'
+fromLang = 'en'
+toLang = 'zh'
+salt = random.randint(32768, 65536)
+
+sign = appid + q + str(salt) + secretKey
+m1 = md5.new()
+m1.update(sign)
+sign = m1.hexdigest()
+myurl = myurl + '?appid=' + appid + '&q=' + urllib.quote(q) + '&from=' + fromLang + '&to=' + toLang + '&salt=' + str(
+    salt) + '&sign=' + sign
+
+try:
+    # httpClient = httplib.HTTPConnection('api.fanyi.baidu.com')
+    # httpClient.request('GET', myurl)
+
+    response=requests.get(myurl)
+
+    # response是HTTPResponse对象
+    # response = httpClient.getresponse()
+    # print response.read()
+    print response.text()
+except Exception, e:
+    print e
+finally:
+    if httpClient:
+        httpClient.close()
